163f8fcbf4d0614ac630fc5414551135a2cc93f2
pranii266 <pranithamuluguru@gmail.com>
2025-08-25 11:02:47 +0530

    model training

---
+++ src/utils.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+import dill
+from sklearn.metrics import r2_score
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok = True)
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for model_name, model in models.items():   # iterate over dict properly
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            # save both scores for debugging (optional)
+            report[model_name] = test_model_score
+            print(f"{model_name} - Train R2: {train_model_score:.3f}, Test R2: {test_model_score:.3f}")  
+        return report  
+    except Exception as e:
+        raise CustomException(e,sys)  
+
+
+
+
+
